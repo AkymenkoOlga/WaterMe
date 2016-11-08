542a6f2407605c56d811f44202f5f71047c7773e
EluTodti <lilchris1310@aol.com>
2016-11-09 00:55:36 +0100

    created layout activity_settings

--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/settings.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/settings.java
@@ -4,20 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainMenu extends AppCompatActivity {
+public class settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-
-    }
-    public void goto_settings(View view){
         setContentView(R.layout.activity_settings);
     }
-
-/*   public void button_name(View view){    insert button_name
-        setContentView(R.layout.activity_name);  insert activity_name
-    }*/
-
+    public void goto_main_menu(View view){
+        setContentView(R.layout.activity_main_menu);
+    }
 }
