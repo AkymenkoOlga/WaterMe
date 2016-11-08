91e75da606dec35b7f534f687edab783c9f5d95c
EluTodti <lilchris1310@aol.com>
2016-11-08 13:04:50 +0100

    Layout fix
    deleted percentage section of textview (layout: activity_main_menu)

--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/MainMenu.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/MainMenu.java
@@ -2,6 +2,7 @@ package com.project.gta.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -11,4 +12,8 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
     }
+/*   public void button_name(View view){    insert button_name
+        setContentView(R.layout.activity_name);  insert activity_name
+    }*/
+
 }
