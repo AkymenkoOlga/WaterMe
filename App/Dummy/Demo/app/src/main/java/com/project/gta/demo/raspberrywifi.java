4b6f26b2f8c3c2e3e795b0480a6332f2db0750bb
EluTodti <lilchris1310@aol.com>
2016-11-14 13:49:46 +0100

    Wifi Menu added
    :heart:  :peach:

---
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/raspberrywifi.java
@@ -0,0 +1,16 @@
+package com.project.gta.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class raspberrywifi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raspberrywifi);
+    }
+}
--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/settings.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/settings.java
@@ -1,14 +1,29 @@
 package com.project.gta.demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class settings extends AppCompatActivity {
+public class settings extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Button button = (Button) findViewById(R.id.raspberrywifi);
+
+        /* dem Button muss gesagt werden, dass er die laufende Activity (MainMenu) als seinen
+        OnClickListener verwendet */
+
+        button.setOnClickListener(this); //this = Refernez aufs aktuelle Object -> die laufende Activity
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, raspberrywifi.class));
+        /* Intent-Object definiert, welche Klasse Android
+        für die zu startende Activity verwenden soll*/
     }
 }
