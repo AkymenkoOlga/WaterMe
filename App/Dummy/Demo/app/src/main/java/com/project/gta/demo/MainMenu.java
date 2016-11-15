4fedafb2e3afc9c86b2ea3470611ab017db2812e
EluTodti <lilchris1310@aol.com>
2016-11-15 17:11:49 +0100

    improved menu
    help and about menu are improved now:
    added titlebar (parentactivity...)
    linear layout
    
    added switch  for startActivity (MainMenu)

--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/MainMenu.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/MainMenu.java
@@ -14,17 +14,35 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        Button button = (Button) findViewById(R.id.settings);
-
+        Button settingsB = (Button) findViewById(R.id.settings);
+        Button helpB = (Button) findViewById(R.id.help);
+        Button aboutB = (Button) findViewById(R.id.about);
+        Button myplantsB = (Button) findViewById(R.id.myplants);
         /* dem Button muss gesagt werden, dass er die laufende Activity (MainMenu) als seinen
         OnClickListener verwendet */
 
-        button.setOnClickListener(this); //this = Refernez aufs aktuelle Object -> die laufende Activity
+        settingsB.setOnClickListener(this); //this = Refernez aufs aktuelle Object -> die laufende Activity#
+        helpB.setOnClickListener(this);
+        aboutB.setOnClickListener(this);
+        myplantsB.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
-    startActivity(new Intent(this,settings.class));
+        switch(v.getId()){
+            case R.id.settings:
+                startActivity(new Intent(this,settings.class));
         /* Intent-Object definiert, welche Klasse Android
         für die zu startende Activity verwenden soll*/
+                break;
+            case R.id.help:
+                startActivity(new Intent(this,help_menu.class));
+                break;
+            case R.id.about:
+                startActivity(new Intent(this,about.class));
+                break;
+            case R.id.myplants:
+                startActivity(new Intent(this,PlantMenu.class));
+                break;
+        }
     }
 }
---
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/help_menu.java
@@ -0,0 +1,15 @@
+package com.project.gta.demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class help_menu extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_menu);
+    }
+}
