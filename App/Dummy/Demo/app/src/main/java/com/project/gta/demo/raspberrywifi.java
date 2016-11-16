50975cf9655acf78d667ee9be01c492b4e6c9077
EluTodti <lilchris1310@aol.com>
2016-11-16 12:39:53 +0100

    Submit toast
    added toast onclick method for submit button in raspberry wifi

--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/raspberrywifi.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/raspberrywifi.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class raspberrywifi extends AppCompatActivity {
 
@@ -12,5 +13,11 @@ public class raspberrywifi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_raspberrywifi);
+    }
+    public void onButtonTapSubmit(View V)   //Displays a toast(message) on the bottom of the display
+    {
+        Toast myToast = Toast.makeText
+                (getApplicationContext(),"Submitting data...",Toast.LENGTH_LONG);
+        myToast.show();
     }
 }
