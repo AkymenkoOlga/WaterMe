2c9e536508b7fab6e1ed4356cd5c9cef0166bb6c
AkymenkoOlga <o.akymenko@gmail.com>
2016-11-20 01:07:45 +0100

    PlantMenu
    Es sollte funktionieren, aber anscheinend steht irgendwo ein Fehler.
    Fünf Pflanzen bit Bildern.

---
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/PlantList.java
@@ -0,0 +1,8 @@
+package com.project.gta.demo;
+
+/**
+ * Created by TInf on 20.11.2016.
+ */
+
+public class PlantList {
+}
--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/PlantMenu.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/PlantMenu.java
@@ -1,13 +1,51 @@
 package com.project.gta.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.app.Activity;
 
-public class PlantMenu extends AppCompatActivity {
+public class PlantMenu extends Activity {
+    ListView list;
+    String[] plants = {
+            "Plant1",
+            "Plant2",
+            "Plant3",
+            "Plant4",
+            "Plant5"
+
+    } ;
+    Integer[] imageId = {
+            R.drawable.plant1,
+            R.drawable.plant2,
+            R.drawable.plant3,
+            R.drawable.plant4,
+            R.drawable.plant5
+
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_menu);
+
+        PlantList adapter = new
+                PlantList(PlantMenu.this, plants, imageId);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(PlantMenu.this, "You Clicked at " +plants[+ position], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
+
 }
