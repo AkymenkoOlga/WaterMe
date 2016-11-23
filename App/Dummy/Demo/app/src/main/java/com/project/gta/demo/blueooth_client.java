0b39ad8f216e2f50fe0e8eaedaca4c394faa3ece
EluTodti <lilchris1310@aol.com>
2016-11-23 11:30:40 +0100

    bt_client, -plantlist
    deleted plant list
    implemented bluetooth client
    created espresso test

---
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/blueooth_client.java
@@ -0,0 +1,191 @@
+package com.project.gta.demo;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+/**
+ * Created by Chris Todt on 21.11.2016.
+ */
+
+class blueooth_client extends Activity implements CompoundButton.OnCheckedChangeListener{
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice = null;
+
+    final byte delimiter = 33;
+    int readBufferPosition = 0;
+    final Handler handler = new Handler();
+
+
+    public void sendBtMsg(String msg2send){
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
+        //UUID uuid = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee"); //Standard SerialPortService ID
+        try {
+
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+            if (!mmSocket.isConnected()){
+                mmSocket.connect();
+                Log.e("Trying to connect","");
+            }
+
+            String msg = msg2send;
+            //msg += "\n";
+            OutputStream mmOutputStream = mmSocket.getOutputStream();
+            mmOutputStream.write(msg.getBytes());
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public class workerThread implements Runnable {
+
+        private String btMsg;
+
+        public workerThread(String msg) {
+            btMsg = msg;
+        }
+
+        public void run()
+        {
+            sendBtMsg(btMsg);
+            while(!Thread.currentThread().isInterrupted())
+            {
+                int bytesAvailable;
+                boolean workDone = false;
+
+                try {
+
+
+
+                    final InputStream mmInputStream;
+                    mmInputStream = mmSocket.getInputStream();
+                    bytesAvailable = mmInputStream.available();
+                    if(bytesAvailable > 0)
+                    {
+
+                        byte[] packetBytes = new byte[bytesAvailable];
+                        Log.e("BT recv bt","bytes available");
+                        byte[] readBuffer = new byte[1024];
+                        mmInputStream.read(packetBytes);
+
+                        for(int i=0;i<bytesAvailable;i++)
+                        {
+                            byte b = packetBytes[i];
+                            if(b == delimiter)
+                            {
+                                byte[] encodedBytes = new byte[readBufferPosition];
+                                System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                final String data = new String(encodedBytes, "US-ASCII");
+                                readBufferPosition = 0;
+
+                                //The variable data now contains our full command
+                                handler.post(new Runnable()
+                                {
+                                    public void run()
+                                    {
+                                     //   myLabel.setText(data);
+                                    }
+                                });
+
+                                workDone = true;
+                                break;
+
+
+                            }
+                            else
+                            {
+                                readBuffer[readBufferPosition++] = b;
+                            }
+                        }
+
+                        if (workDone == true){
+                            mmSocket.close();
+                            break;
+                        }
+
+                    }
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+     //   final TextView myLabel = (TextView) findViewById(R.id.btResult);
+        final Switch ledSW = (Switch) findViewById(R.id.SWled);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
+
+        //Implement Switches here
+        //Add Switch in onCheckedChanged function below
+
+        //LED Switch
+        ledSW.setOnCheckedChangeListener(this);
+
+
+
+/*        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+*/
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device : pairedDevices)
+            {
+                if(device.getName().equals("raspberrypi")) //Note, you will need to change this to match the name of your device
+                {
+                 //   Log.e("Aquarium",device.getName());
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+
+
+    }
+
+    //CheckedChangeListener
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch(buttonView.getId()) {
+            case (R.id.SWled):
+                (new Thread(new workerThread("LEDon"))).start();
+                break;
+
+        }
+
+    }
+        //@Override
+   // public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+      //  getMenuInflater().inflate(R.menu.main, menu);
+     //   return true;
+    }
+
+
+
--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/bluetooth.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/bluetooth.java
@@ -1,10 +1,15 @@
 package com.project.gta.demo;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -21,15 +26,26 @@ public class bluetooth extends AppCompatActivity implements CompoundButton.OnChe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
 
+        //Array für gefundene bluetooth devices
+        ArrayAdapter<String> btArrayAdapter
+                = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1);
+
+        //Bluetooth switch
         Switch bluetoothSw = (Switch) findViewById(R.id.bluetoothsw);
         bluetoothSw.setOnCheckedChangeListener(this);
 
-        //Button o list_paired_devices activity
+        //Button for list_paired_devices activity
         Button listpaireddevicesB = (Button) findViewById(R.id.listpaireddevices);
+        listpaireddevicesB.setOnClickListener(this);
+
+        //Button for new devices
+        Button listfounddevicesB = (Button) findViewById(R.id.listfounddevices);
+        listfounddevicesB.setOnClickListener(this);
+
         //Bluetooth adapter
         BA = BluetoothAdapter.getDefaultAdapter();
 
-        listpaireddevicesB.setOnClickListener(this);
 
     }
     @Override
@@ -57,8 +73,43 @@ public class bluetooth extends AppCompatActivity implements CompoundButton.OnChe
 
     @Override
     public void onClick(View v) {
-        startActivity(new Intent(this,ListPairedDevicesActivity.class));
+        switch(v.getId()){
+            //Button for listpaireddevices is clicked
+            case R.id.listpaireddevices:
+                startActivity(new Intent(this,ListPairedDevicesActivity.class));
+                break;
+            //Button for listfounddevices is clicked
+            case R.id.listfounddevices:
+                newdevices();
+                break;
+
+        }
+
     }
-}
+
+    //testfunction for discovering ne devices
+    public void newdevices() {
+        BA.startDiscovery();
+        // Create a BroadcastReceiver for ACTION_FOUND
+        BroadcastReceiver mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                // When discovery finds a device
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    // Get the BluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    // Add the name and address to an array adapter to show in a ListView
+                   // btArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            }
+        };
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+        BA.cancelDiscovery();
+        }
+
+    }
+
 
 
