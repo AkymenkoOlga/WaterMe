7543e768a42b403bfda3a7059bbe12997b95d17b
PaulGiesa <paul.giesa@arcor.de>
2016-11-26 21:31:58 +0100

    Update server.py

--- Pi/server.py
+++ Pi/server.py
@@ -1,31 +1,64 @@
 import socket
 import RPi.GPIO as GPIO
+import spidev
+import os
+import time
+
+delay = 0.2
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+def readChannel(channel):
+  val = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((val[1]&3) << 8) + val[2]
+  return data
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 
-hostMACAdress = '00:15:83:E7:b4:A5'
+hostMACAddress = '00:15:83:E7:B4:A5'
 port = 3
 backlog = 1
 size = 1024
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.bind(hostMACAddress,port)
+s.bind((hostMACAddress,port))
 s.listen(backlog)
+print "Waiting for connection on RFCOMM channel %d" % port
+
 try:
-  client, address = s.accept()
-  print "Client connected"
-  while 1:
-    data= client.rec(size)
-    if data:
-      print(data)
-    if data == "on":
-      print "turning LEDs on"
-      GPIO.output(2, GPIO.HIGH)
-    if data == "off":
-      print "turning LEDs off"
-      GPIO.output(2, GPIO.LOW)
+    client, address = s.accept()
+    print "Accepted connection from", address
+
+    while 1:
+        data = client.recv(size)
+        if data == "on":
+            GPIO.output(2,GPIO.HIGH)
+            GPIO.output(3,GPIO.HIGH)
+            GPIO.output(4,GPIO.HIGH)
+            print "turning LEDs on"
+            client.send('hurensohn')
+        if data == "off":
+            GPIO.output(2,GPIO.LOW)
+            GPIO.output(3,GPIO.LOW)
+            GPIO.output(4,GPIO.LOW)
+            print "turning LEDs off"
+            client.send('mendelsohn')
+        if data == "wet":
+                if __name__ == "__main__":
+                 try:
+                        val = readChannel(0)
+                        if (val != 0):
+                         print(val)
+                        client.send(str(val))
+                        time.sleep(delay)
+
+                 except KeyboardInterrupt:
+                  print "Cancel."
+
 
 except:
-  print "Closing socket"
-  client.close()
-  s.close()
+    print("Closing socket")
+    client.close()
+    s.close()
