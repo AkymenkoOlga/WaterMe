11b4edeb1b206d5f579a1c2be7d4fcc5ed874f62
EluTodti <lilchris1310@aol.com>
2016-12-05 13:52:36 +0100

    Update help menu
    prototype of help menu
    BT-PI
    install Pi

--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/HelpMenu.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/HelpMenu.java
@@ -1,15 +1,39 @@
 package com.project.gta.demo;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 
-public class HelpMenu extends AppCompatActivity{
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class HelpMenu extends AppCompatActivity implements View.OnClickListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_menu);
+
+
+        Button BTNhelpBluetoothPi = (Button) findViewById(R.id.BTNhelpBluetoothPi);
+        BTNhelpBluetoothPi.setOnClickListener(this);
+
+
     }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.BTNhelpBluetoothPi):
+                setContentView(R.layout.help_bluetooth_pi);
+                break;
+            case (R.id.BTNhelpInstallPi):
+            setContentView(R.layout.help_install_pi);
+                break;
+        }
+    }
+
 }
+
+
