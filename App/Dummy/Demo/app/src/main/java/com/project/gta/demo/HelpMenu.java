bc4aa35b584c57ae5813e7c9898403fd4c81b776
EluTodti <lilchris1310@aol.com>
2016-12-06 15:47:00 +0100

    Updated Help Menu
    created add plant (2nd semester)
    fixed bluetooth_pi : static orientation
    updated install_pi : image
    implemeted ask_us: webview to contact page + anddroid back button will
    load previous page of webview

--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/HelpMenu.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/HelpMenu.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -20,6 +22,16 @@ public class HelpMenu extends AppCompatActivity implements View.OnClickListener{
         Button BTNhelpBluetoothPi = (Button) findViewById(R.id.BTNhelpBluetoothPi);
         BTNhelpBluetoothPi.setOnClickListener(this);
 
+        Button BTNhelpInstallPi = (Button) findViewById(R.id.BTNhelpInstallPi);
+        BTNhelpInstallPi.setOnClickListener(this);
+
+        Button BTNhelpAddPlant = (Button) findViewById(R.id.BTNhelpAddPlant);
+        BTNhelpAddPlant.setOnClickListener(this);
+
+        Button BTNhelpAskUs = (Button) findViewById(R.id.BTNhelpAskus);
+        BTNhelpAskUs.setOnClickListener(this);
+
+
 
     }
     @Override
@@ -29,7 +41,13 @@ public class HelpMenu extends AppCompatActivity implements View.OnClickListener{
                 setContentView(R.layout.help_bluetooth_pi);
                 break;
             case (R.id.BTNhelpInstallPi):
-            setContentView(R.layout.help_install_pi);
+                setContentView(R.layout.help_install_pi);
+                break;
+            case (R.id.BTNhelpAddPlant):
+                setContentView(R.layout.help_add_plant);
+                break;
+            case (R.id.BTNhelpAskus):
+                startActivity(new Intent(this, WVaskus.class));
                 break;
         }
     }
---
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/WVaskus.java
@@ -0,0 +1,39 @@
+package com.project.gta.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WVaskus extends AppCompatActivity {
+
+
+    private WebView WEBask_us;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wvaskus);
+
+        WEBask_us = (WebView) findViewById(R.id.WEBask_us);
+        // Enable Javascript
+        WebSettings webSettings = WEBask_us.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        WEBask_us.loadUrl("http://gtaproject.wordpress.com/contact");
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        WEBask_us.setWebViewClient(new WebViewClient());
+    }
+
+
+    // Prevents activity from closing when pressing android back button
+    // Also enables going back in WebView
+    @Override
+    public void onBackPressed() {
+        if(WEBask_us.canGoBack()) {
+            WEBask_us.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
