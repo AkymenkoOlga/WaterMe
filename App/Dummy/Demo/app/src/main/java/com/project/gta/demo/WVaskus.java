6bff3f40a832fd154cf5be3ff38305017e3ba602
EluTodti <lilchris1310@aol.com>
2017-04-04 10:06:48 +0200

    added toast
    added connection toast for ask us page

--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/WVaskus.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/WVaskus.java
@@ -1,24 +1,41 @@
 package com.project.gta.demo;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class WVaskus extends AppCompatActivity {
 
 
     private WebView WEBask_us;
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(connection()){
+
+        }else
+        {
+            Toast toast_notifications_enabled = Toast.makeText
+                    (getApplicationContext(), "Internet disconnected", Toast.LENGTH_LONG);
+            toast_notifications_enabled.show();
+        }
+
         setContentView(R.layout.activity_wvaskus);
 
         WEBask_us = (WebView) findViewById(R.id.WEBask_us);
         // Enable Javascript
         WebSettings webSettings = WEBask_us.getSettings();
         webSettings.setJavaScriptEnabled(false);
+
         WEBask_us.loadUrl("http://gtaproject.wordpress.com/contact");
 
         // Force links and redirects to open in the WebView instead of in a browser
@@ -36,4 +53,16 @@ public class WVaskus extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+    public boolean connection() {
+        boolean connected = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            connected = true;
+        } else
+            connected = false;
+        return connected;
+    }
+
 }
