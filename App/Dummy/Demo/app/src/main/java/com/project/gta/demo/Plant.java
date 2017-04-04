453a426be0c488cf8ce2e5947e6425d3c05cbb4b
EluTodti <lilchris1310@aol.com>
2017-04-04 11:44:43 +0200

    added class
    Plant class added for future plant menu

---
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/Plant.java
@@ -0,0 +1,14 @@
+package com.project.gta.demo;
+
+
+
+public class Plant {
+    public void Plant(String Name, double Humidity){
+        name = Name;
+        humidity = Humidity;
+    }
+
+    double humidity;
+    String name;
+
+}
