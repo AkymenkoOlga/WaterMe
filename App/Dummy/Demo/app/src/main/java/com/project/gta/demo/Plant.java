e3218ceee6ae544d66ff6869b73525bd4c508a5c
EluTodti <lilchris1310@aol.com>
2017-05-30 12:21:52 +0200

    fix

--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/Plant.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/Plant.java
@@ -3,8 +3,8 @@ package com.project.gta.demo;
 
 
 public class Plant {
-    private double humidity;
-    private String name;
+    public double humidity;
+    public String name;
 
     public void plant(String Name, double Humidity){
         name = Name;
