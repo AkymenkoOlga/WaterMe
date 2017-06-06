a2e2408839156914b30ab02391daed90a9e8ab22
EluTodti <lilchris1310@aol.com>
2017-06-06 14:03:27 +0200

    Add changePlantImage

--- App/Dummy/Demo/app/src/main/java/com/project/gta/demo/PlantSelect.java
+++ App/Dummy/Demo/app/src/main/java/com/project/gta/demo/PlantSelect.java
@@ -1,35 +1,103 @@
 package com.project.gta.demo;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class PlantSelect extends AppCompatActivity implements View.OnClickListener{
 
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
     private String PlantName;
+    private static int LOAD_IMAGE_RESULTS = 1;
+    private String imgpath;
+
+    private ImageView image;
+    private FloatingActionButton fabEdit;
+    private FloatingActionButton fabCheck;
+    private TextView plant1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_select);
 
-        TextView plant1 = (TextView) findViewById(R.id.tvPlant1);
-        plant1.setText(readDataPlant1());
+        plant1 = (TextView) findViewById(R.id.tvPlant1);
+        plant1.setText(readDataPlant1("text"));
 
-        FloatingActionButton fabEdit = (FloatingActionButton) findViewById(R.id.FABplantEdit);
+        fabEdit = (FloatingActionButton) findViewById(R.id.FABplantEdit);
         fabEdit.setOnClickListener(this);
-        FloatingActionButton fabCheck = (FloatingActionButton) findViewById(R.id.FABedit_plant_check);
+        fabCheck = (FloatingActionButton) findViewById(R.id.FABedit_plant_check);
         fabCheck.setOnClickListener(this);
 
         LinearLayout editName = (LinearLayout) findViewById(R.id.EditName);
         editName.setVisibility(View.GONE);
 
+        image = (ImageView) findViewById(R.id.image);
+        image.setOnClickListener(this);
+
+        verifyStoragePermissions(this);
+
+        if(readDataPlant1("image") != ""){
+            image.setImageBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeFile(readDataPlant1("image")),200,200,false));
+        }
+    }
+
+
+    //Storage Permission for Android API23+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == LOAD_IMAGE_RESULTS && resultCode == RESULT_OK && data != null) {
+            Uri pickedImage = data.getData();
+
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+            writeImagePlant1(imagePath);
+
+            cursor.close();
+
+            image.setImageBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeFile(imagePath),200,200,false));
+            //image.setImageURI(pickedImage)
+        }
     }
 
     @Override
@@ -47,15 +115,16 @@ public class PlantSelect extends AppCompatActivity implements View.OnClickListen
                 break;
             case R.id.FABedit_plant_check:
                 writeDataPlant1();
-                readDataPlant1();
+                readDataPlant1("text");
                 LinearLayout checkName = (LinearLayout) findViewById(R.id.EditName);
                 checkName.setVisibility(View.GONE);
 
-                TextView plant1 = (TextView) findViewById(R.id.tvPlant1);
-                plant1.setText(readDataPlant1());
+                plant1.setText(readDataPlant1("text"));
 
-                FloatingActionButton fabEditShow = (FloatingActionButton) findViewById(R.id.FABplantEdit);
-                fabEditShow.setVisibility(View.VISIBLE);
+                fabEdit.setVisibility(View.VISIBLE);
+            case R.id. image:
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, LOAD_IMAGE_RESULTS);
             default:
                 //Nothing
         }
@@ -73,9 +142,27 @@ public class PlantSelect extends AppCompatActivity implements View.OnClickListen
         }
     }
 
-    private String readDataPlant1() {
+    public void writeImagePlant1(String img){
         SharedPreferences pref = getSharedPreferences("Plant1", 0);
-        return pref.getString("Plant1Name", "MyPlant"); //2.Param = Default, falls k
+        SharedPreferences.Editor editor = pref.edit();
+        imgpath = img;
+        if (!imgpath.equals("")) {
+            editor.putString("Plant1Image", imgpath);
+            editor.apply();
+        }
+    }
+
+    private String readDataPlant1(String string) {
+        if(string.equals("text")) {
+            SharedPreferences pref = getSharedPreferences("Plant1", 0);
+            return pref.getString("Plant1Name", "MyPlant"); //2.Param = Default, falls k
+        }
+        if (string.equals("image"))
+        {
+            SharedPreferences pref = getSharedPreferences("Plant1",0);
+            return pref.getString("Plant1Image", "");
+        }
+        return "";
     }
 
 
